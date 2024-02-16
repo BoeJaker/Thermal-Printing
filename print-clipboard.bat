42cd4926a50130d2f11bf0d0ec9171162a15e54e
Boe Jaker <j.baker.cwp@gmail.com>
2024-02-16 17:10:53 +0000

    Update print-clipboard.bat
    Changed curl request to python script

--- print-clipboard.bat
+++ print-clipboard.bat
@@ -1,9 +1,9 @@
 @echo off
 
 REM Use PowerShell to retrieve the clipboard text and encode it for URL
-for /f "delims=" %%A in ('powershell -command "$clip = Get-Clipboard; $clip"') do set "encoded_clipboard=%%A"
+for /f "delims=" %%A in ('powershell -command "$clip = Get-Clipboard; $clip"') do set "clipboard=%%A"
 
 REM Use curl to send the encoded content to the server
-curl -X GET "http://192.168.3.201:12345/print/%encoded_clipboard%"
+python3 C:\Users\User\MEGA\send_message.py -t %clipboard%
 
 pause
